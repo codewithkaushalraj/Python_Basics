cb28103bec5ee011b62dd950d429bd2183243cb8
Kaushal Raj <Kaushalrajverma2007@gmail.com>
2026-04-14 19:03:28 +0530

    array in python

---
+++ array In Python/arrayInPython.py
@@ -0,0 +1,16 @@
+# from array import *;
+
+# arr=array('i',[45,5,6,9,8,7,4,5,6,3,10,15])
+
+# for ele in arr:
+#     print(ele,end=" ")
+# print(type(arr))
+
+# arr.append(50)
+# arr.append(100)
+# print(arr)
+# print(arr.buffer_info())
+# print(len(arr))
+# # arr.clear() # delete all element in array
+# print(arr)
+
